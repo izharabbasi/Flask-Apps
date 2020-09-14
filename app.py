e97ba57dfaa48f3688191f8f39c88196ec8e0e7c
izharabbasi <izharabbasi103@gmail.com>
2020-09-15 01:23:05 +0500

    add variables

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello! You Are On The Main Page </h1>'
-@app.route('/home')
-def home():
-    return '<h1>Hello you are on the Home Page</h1>'
+
+@app.route('/home',methods=['POST','get'], defaults={'name':'izhar'})
+@app.route('/home/<name>', methods=['POST','GET'])
+def home(name):
+    return '<h1>Hello {}, you are on the Home Page</h1>'.format(name)
+
 
 @app.route('/Profile')
 def profile():
@@ -18,4 +21,4 @@ def json():
     return jsonify({'key':'value', 'key2':[1,2,3,4]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
