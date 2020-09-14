cb6b0226c8326a13c386cf21c3b79b7df12c5d28
izharabbasi <izharabbasi103@gmail.com>
2020-09-15 00:17:51 +0500

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/<name>')
 def index(name):
-    return '<h1>Hello!, {} </h1>'.format(name)
+    return '<h1>Hello! {} </h1>'.format(name)
 
 
 if __name__ == '__main__':
